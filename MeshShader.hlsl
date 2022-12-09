fd5addda390a4dc5a9b58c08eb04db48f45aa8aa
clandrew <clandrew@microsoft.com>
2022-12-08 17:46:07 -0800

    Fix busted shader

--- MeshShader.hlsl
+++ MeshShader.hlsl
@@ -21,69 +21,46 @@ void MSMain(
 	out vertices MSvert verts[8],
 	out indices uint3 idx[12]) // Three indices per primitive
 {
-	int i;
 	float4 v;
 	const uint numVertices = 8;
 	const uint numPrimitives = 12;
 
 	SetMeshOutputCounts(numVertices, numPrimitives);
-	
-	v = float4(-0.5f, -0.5f, -0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[0].pos = v;
-	verts[0].color = float3(0.0f, 0.0f, 0.0f);
 
-	v = float4(-0.5f, -0.5f, 0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[1].pos = v;
-	verts[1].color = float3(0.0f, 0.0f, 1.0f);
+	float4 cubeVertexPositions[] =
+	{
+		-0.5f, -0.5f, -0.5f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f
+	};
 
-	v = float4(-0.5f, 0.5f, -0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[2].pos = v;
-	verts[2].color = float3(0.0f, 1.0f, 0.0f);
+	float3 cubeVertexColors[] =
+	{
+		0.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 1.0f, 1.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 1.0f,
+		1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 1.0f
+	};
 
-	v = float4(-0.5f, 0.5f, 0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[3].pos = v;
-	verts[3].color = float3(0.0f, 1.0f, 1.0f);
+	for (int i = 0; i < 8; ++i)
+	{
+		float4 v = cubeVertexPositions[i];
+		v = mul(v, model);
+		v = mul(v, view);
+		v = mul(v, projection);
+		verts[i].pos = v;
+		verts[i].color = cubeVertexColors[i];
+	}
 
-	v = float4(0.5f, -0.5f, -0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[4].pos = v;
-	verts[4].color = float3(1.0f, 0.0f, 0.0f);
-
-	v = float4(0.5f, -0.5f, 0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[5].pos = v;
-	verts[5].color = float3(1.0f, 0.0f, 1.0f);
-
-	v = float4(0.5f, 0.5f, -0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[6].pos = v;
-	verts[6].color = float3(1.0f, 1.0f, 0.0f);
-
-	v = float4(0.5f, 0.5f, 0.5f, 0.0f);
-	v = mul(v, model);
-	v = mul(v, view);
-	v = mul(v, projection);
-	verts[7].pos = v;
-	verts[7].color = float3(1.0f, 1.0f, 1.0f);
-	
 	uint cubeIndices[] =
 	{
 		0, 2, 1, // -x
